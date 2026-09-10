34d469d4b486386d4b8e8cec98a68c2a73b63707
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 23:45:48 +0100

    feat: wire Stage 1C core services

--- resources/[himo]/himo_core/fxmanifest.lua
+++ resources/[himo]/himo_core/fxmanifest.lua
@@ -3,12 +3,13 @@ game 'gta5'
 
 name 'himo_core'
 author 'HimotheeGaming'
-description 'HimotheeCore framework foundation'
-version '0.3.2'
+description 'HimotheeCore modular roleplay framework core services'
+version '0.4.0'
 
 lua54 'yes'
 
 shared_scripts {
+    '@ox_lib/init.lua',
     'shared/config.lua'
 }
 
@@ -16,17 +17,29 @@ server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/logger.lua',
     'server/database.lua',
+    'server/state.lua',
+    'server/callbacks.lua',
+    'server/permissions.lua',
+    'server/commands.lua',
+    'server/sessions.lua',
     'server/identifiers.lua',
-    'server/characters.lua',
     'server/money.lua',
+    'server/jobs.lua',
+    'server/metadata.lua',
+    'server/characters.lua',
     'server/player.lua',
     'server/lifecycle.lua',
+    'server/admin.lua',
     'server/main.lua'
 }
 
 client_scripts {
     'client/main.lua',
+    'client/callbacks.lua',
     'client/lifecycle.lua'
 }
 
-dependency 'oxmysql'
+dependencies {
+    'ox_lib',
+    'oxmysql'
+}
