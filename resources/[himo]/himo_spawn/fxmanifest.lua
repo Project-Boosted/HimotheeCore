ef998b94d9d86d3abc9325e3f72fa3d331855732
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 22:41:35 +0100

    feat: add himo_spawn manifest

---
+++ resources/[himo]/himo_spawn/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'himo_spawn'
+author 'HimotheeGaming'
+description 'HimotheeCore spawn selection and handoff'
+version '0.3.0'
+
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
+
+client_script 'client/main.lua'
+
+dependencies {
+    'ox_lib',
+    'himo_core'
+}
