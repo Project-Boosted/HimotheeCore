1e4f596d39ec3bd2ce6381b54cabf6bf7d23b834
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 16:23:24 +0100

    feat: add himo_core shared configuration

---
+++ resources/[himo]/himo_core/shared/config.lua
@@ -0,0 +1,9 @@
+HimoConfig = HimoConfig or {}
+
+HimoConfig.FrameworkName = 'HimotheeCore'
+HimoConfig.Version = '0.1.0'
+HimoConfig.RequiredSchemaVersion = 1
+HimoConfig.Debug = GetConvarInt('himo:debug', 0) == 1
+HimoConfig.MaxCharacters = math.max(1, GetConvarInt('himo:maxCharacters', 4))
+HimoConfig.StartingCash = math.max(0, GetConvarInt('himo:startingCash', 500))
+HimoConfig.StartingBank = math.max(0, GetConvarInt('himo:startingBank', 5000))
