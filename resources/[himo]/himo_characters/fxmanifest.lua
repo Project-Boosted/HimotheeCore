8303dee28644395014b33bfcabb492d50ab995cd
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 19:01:53 +0100

    feat: add Stage 1B himo_characters resource manifest

---
+++ resources/[himo]/himo_characters/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'himo_characters'
+author 'HimotheeGaming'
+description 'HimotheeCore multicharacter selection, creation and spawn lifecycle'
+version '0.2.0'
+
+lua54 'yes'
+
+ui_page 'web/index.html'
+
+files {
+    'web/index.html',
+    'web/style.css',
+    'web/app.js'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+dependencies {
+    'himo_core',
+    'spawnmanager'
+}
