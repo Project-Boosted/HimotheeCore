12aaea5808072af753c6055fc8849af7e9cfd4b1
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 19:01:08 +0100

    feat: add Stage 1B autosave config

--- resources/[himo]/himo_core/shared/config.lua
+++ resources/[himo]/himo_core/shared/config.lua
@@ -1,9 +1,10 @@
 HimoConfig = HimoConfig or {}
 
 HimoConfig.FrameworkName = 'HimotheeCore'
-HimoConfig.Version = '0.1.4'
+HimoConfig.Version = '0.2.0'
 HimoConfig.RequiredSchemaVersion = 1
 HimoConfig.Debug = GetConvarInt('himo:debug', 0) == 1
 HimoConfig.MaxCharacters = math.max(1, GetConvarInt('himo:maxCharacters', 4))
 HimoConfig.StartingCash = math.max(0, GetConvarInt('himo:startingCash', 500))
 HimoConfig.StartingBank = math.max(0, GetConvarInt('himo:startingBank', 5000))
+HimoConfig.AutoSaveMs = math.max(15000, GetConvarInt('himo:autoSaveMs', 60000))
