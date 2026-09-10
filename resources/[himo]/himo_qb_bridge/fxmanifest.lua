ef7a922993b05ee27c17a9591c8873108f9494b8
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 23:48:42 +0100

    feat: update QB bridge for Stage 1C

--- resources/[himo]/himo_qb_bridge/fxmanifest.lua
+++ resources/[himo]/himo_qb_bridge/fxmanifest.lua
@@ -3,14 +3,18 @@ game 'gta5'
 
 name 'himo_qb_bridge'
 author 'HimotheeGaming'
-description 'Limited QB compatibility bridge for HimotheeCore third-party resources'
-version '0.3.2'
+description 'Limited QB compatibility bridge backed by HimotheeCore native services'
+version '0.4.0'
 
 lua54 'yes'
 
 provide 'qb-core'
 
+shared_script '@ox_lib/init.lua'
 client_script 'client/main.lua'
 server_script 'server/main.lua'
 
-dependency 'himo_core'
+dependencies {
+    'ox_lib',
+    'himo_core'
+}
