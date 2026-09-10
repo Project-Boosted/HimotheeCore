304264df4938df983c86fe9b866f4c41b4619f69
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 16:23:14 +0100

    feat: add himo_core resource manifest

---
+++ resources/[himo]/himo_core/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'himo_core'
+author 'HimotheeGaming'
+description 'HimotheeCore framework foundation'
+version '0.1.0'
+
+lua54 'yes'
+
+shared_scripts {
+    'shared/config.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/logger.lua',
+    'server/database.lua',
+    'server/identifiers.lua',
+    'server/characters.lua',
+    'server/money.lua',
+    'server/main.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+dependency 'oxmysql'
