10f6b9e21076e3585d936726bc65dd0b15851d51
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-10 23:07:33 +0100

    feat: add QB compatibility bridge manifest

---
+++ resources/[himo]/himo_qb_bridge/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'himo_qb_bridge'
+author 'HimotheeGaming'
+description 'Limited QB compatibility bridge for HimotheeCore third-party resources'
+version '0.3.1'
+
+lua54 'yes'
+
+provide 'qb-core'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+dependency 'himo_core'
