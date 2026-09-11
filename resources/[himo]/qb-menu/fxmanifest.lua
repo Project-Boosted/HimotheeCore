14c0756ab9118233bfa321e4344d46d0d113e39f
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 11:04:20 +0100

    release: bump qb-menu facade to v0.5.5

--- resources/[himo]/qb-menu/fxmanifest.lua
+++ resources/[himo]/qb-menu/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qb-menu'
 author 'HimotheeGaming'
 description 'qb-menu compatibility facade backed by ox_lib contexts'
-version '0.5.4'
+version '0.5.5'
 
 lua54 'yes'
 shared_script '@ox_lib/init.lua'
