25ac9f858979da7a44ebf3051aa951e8dc938909
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 11:04:57 +0100

    release: bump progressbar facade to v0.5.5

--- resources/[himo]/progressbar/fxmanifest.lua
+++ resources/[himo]/progressbar/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'progressbar'
 author 'HimotheeGaming'
 description 'QBCore progressbar compatibility facade backed by ox_lib'
-version '0.5.4'
+version '0.5.5'
 
 lua54 'yes'
 shared_script '@ox_lib/init.lua'
