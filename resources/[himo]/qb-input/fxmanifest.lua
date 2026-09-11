7e832bb584a65d2e58be04fbeaf8f7818b8d5308
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 03:03:10 +0100

    release: bump qb-input facade to v0.5.2

--- resources/[himo]/qb-input/fxmanifest.lua
+++ resources/[himo]/qb-input/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qb-input'
 author 'HimotheeGaming'
 description 'qb-input compatibility facade backed by ox_lib input dialogs'
-version '0.5.1'
+version '0.5.2'
 
 lua54 'yes'
 shared_script '@ox_lib/init.lua'
