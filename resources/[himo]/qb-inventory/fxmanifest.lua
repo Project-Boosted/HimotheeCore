4dfd7f94ee535c3327f4c4fa5115a4e1d1fa9c96
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:35:59 +0100

    release: bump qb-inventory facade to v0.5.1

--- resources/[himo]/qb-inventory/fxmanifest.lua
+++ resources/[himo]/qb-inventory/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qb-inventory'
 author 'HimotheeGaming'
 description 'qb-inventory compatibility facade backed by ox_inventory'
-version '0.5.0'
+version '0.5.1'
 
 lua54 'yes'
 shared_script '@ox_lib/init.lua'
