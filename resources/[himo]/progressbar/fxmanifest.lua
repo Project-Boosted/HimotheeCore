001eb133e13be8014de647abf3fd2fbd05419dc9
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:08:37 +0100

    feat: add progressbar compatibility facade

---
+++ resources/[himo]/progressbar/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'progressbar'
+author 'HimotheeGaming'
+description 'QBCore progressbar compatibility facade backed by ox_lib'
+version '0.5.0'
+
+lua54 'yes'
+shared_script '@ox_lib/init.lua'
+client_script 'client/main.lua'
+
+dependency 'ox_lib'
