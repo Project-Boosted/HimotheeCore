7c27fc9be5620cd63edf60aa9a9090f3d2530b31
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 18:28:29 +0100

    release: bump himo_appearance to v0.5.9

--- resources/[himo]/himo_appearance/fxmanifest.lua
+++ resources/[himo]/himo_appearance/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'himo_appearance'
 author 'HimotheeGaming'
 description 'HimotheeCore appearance persistence with Illenium bridge'
-version '0.5.8'
+version '0.5.9'
 
 lua54 'yes'
 
