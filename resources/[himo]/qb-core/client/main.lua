40df0146b5934b2e6bd592bb18f553b80aa785c2
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 03:40:07 +0100

    fix: provide QB callback transport in exact qb-core facade

--- resources/[himo]/qb-core/client/main.lua
+++ resources/[himo]/qb-core/client/main.lua
@@ -1,5 +1,41 @@
+local clientCallbacks = {}
+local sharedMirror = {
+    Items = {}, Vehicles = {}, Weapons = {}, Locations = {}, StarterItems = {}, Jobs = {}, Gangs = {}
+}
+
+local function triggerCallback(name, cb, ...)
+    if type(name) ~= 'string' or type(cb) ~= 'function' then return end
+    clientCallbacks[name] = cb
+    TriggerServerEvent('QBCore:Server:TriggerCallback', name, ...)
+end
+
+RegisterNetEvent('QBCore:Client:TriggerCallback', function(name, ...)
+    local cb = clientCallbacks[name]
+    if not cb then return end
+    clientCallbacks[name] = nil
+    cb(...)
+end)
+
+RegisterNetEvent('himo_qb_bridge:client:setShared', function(jobs, gangs, items, vehicles)
+    sharedMirror.Jobs = type(jobs) == 'table' and jobs or sharedMirror.Jobs
+    sharedMirror.Gangs = type(gangs) == 'table' and gangs or sharedMirror.Gangs
+    sharedMirror.Items = type(items) == 'table' and items or sharedMirror.Items
+    sharedMirror.Vehicles = type(vehicles) == 'table' and vehicles or sharedMirror.Vehicles
+end)
+
 local function core()
-    return exports['himo_qb_bridge']:GetCoreObject()
+    local object = exports['himo_qb_bridge']:GetCoreObject()
+    object.Functions = object.Functions or {}
+    object.Shared = object.Shared or {}
+    object.Functions.TriggerCallback = triggerCallback
+
+    for namespace, value in pairs(sharedMirror) do
+        if type(value) == 'table' and next(value) ~= nil then
+            object.Shared[namespace] = value
+        end
+    end
+
+    return object
 end
 
 exports('GetCoreObject', function()
