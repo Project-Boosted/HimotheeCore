ad7e5dc7eca809c7edf099fbe3ec53fba6f84718
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 11:23:28 +0100

    release: set v0.5.6 runtime build metadata

--- resources/[himo]/himo_core/shared/config.lua
+++ resources/[himo]/himo_core/shared/config.lua
@@ -1,10 +1,10 @@
 HimoConfig = HimoConfig or {}
 
 HimoConfig.FrameworkName = 'HimotheeCore'
-HimoConfig.Version = '0.5.5'
+HimoConfig.Version = '0.5.6'
 HimoConfig.RequiredSchemaVersion = 5
 HimoConfig.Stage = '1D'
-HimoConfig.Build = 'qbx-jobs-qbcallback-fix'
+HimoConfig.Build = 'authoritative-qb-qbx-runtime'
 
 HimoConfig.Debug = GetConvarInt('himo:debug', 0) == 1
 HimoConfig.MaxCharacters = math.max(1, GetConvarInt('himo:maxCharacters', 4))
