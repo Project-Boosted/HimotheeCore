6484e7aa4a9733e3dddecb26fbbcd7fdbb902350
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:16:21 +0100

    feat: sync QB shared item and vehicle catalogs to clients

--- resources/[himo]/himo_qb_bridge/client/catalog.lua
+++ resources/[himo]/himo_qb_bridge/client/catalog.lua
@@ -1,7 +1,9 @@
-RegisterNetEvent('himo_qb_bridge:client:setShared', function(jobs, gangs)
+RegisterNetEvent('himo_qb_bridge:client:setShared', function(jobs, gangs, items, vehicles)
     local core = exports['himo_qb_bridge']:GetCoreObject()
     core.Shared.Jobs = type(jobs) == 'table' and jobs or core.Shared.Jobs or {}
     core.Shared.Gangs = type(gangs) == 'table' and gangs or core.Shared.Gangs or {}
+    core.Shared.Items = type(items) == 'table' and items or core.Shared.Items or {}
+    core.Shared.Vehicles = type(vehicles) == 'table' and vehicles or core.Shared.Vehicles or {}
 end)
 
 CreateThread(function()
