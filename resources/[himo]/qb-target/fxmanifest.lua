d435fe5223913dc65c88401d4ba863de7092ea77
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:35:44 +0100

    release: bump qb-target facade to v0.5.1

--- resources/[himo]/qb-target/fxmanifest.lua
+++ resources/[himo]/qb-target/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qb-target'
 author 'HimotheeGaming'
 description 'qb-target compatibility facade backed by ox_target'
-version '0.5.0'
+version '0.5.1'
 
 lua54 'yes'
 shared_script '@ox_lib/init.lua'
