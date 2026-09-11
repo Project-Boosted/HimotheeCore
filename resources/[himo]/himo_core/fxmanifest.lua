2cf0f03f857ff3b611217c73f3fe7ad003a4d576
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 18:26:49 +0100

    release: wire Jim gameplay acceptance into himo_core

--- resources/[himo]/himo_core/fxmanifest.lua
+++ resources/[himo]/himo_core/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'himo_core'
 author 'HimotheeGaming'
 description 'HimotheeCore modular roleplay framework core services'
-version '0.5.8'
+version '0.5.9'
 
 lua54 'yes'
 
@@ -33,6 +33,7 @@ server_scripts {
     'server/lifecycle.lua',
     'server/admin.lua',
     'server/compat_callbacks.lua',
+    'server/jim_acceptance.lua',
     'server/selftest.lua',
     'server/main.lua'
 }
