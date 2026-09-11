f3063d407382c1569e9fe65d8834247259907103
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 01:03:39 +0100

    feat: add real qb-core compatibility facade

---
+++ resources/[himo]/qb-core/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'qb-core'
+author 'HimotheeGaming'
+description 'QBCore compatibility facade backed by HimotheeCore'
+version '0.4.3'
+
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+dependencies {
+    'ox_lib',
+    'himo_core',
+    'himo_qb_bridge'
+}
