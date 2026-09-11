f9997ffb43d09e5f3393ccccca77ce92729612a2
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:14:10 +0100

    release: bump himo_characters to v0.5.0

--- resources/[himo]/himo_characters/fxmanifest.lua
+++ resources/[himo]/himo_characters/fxmanifest.lua
@@ -4,12 +4,11 @@ game 'gta5'
 name 'himo_characters'
 author 'HimotheeGaming'
 description 'HimotheeCore tutorial-session multicharacter lifecycle'
-version '0.4.3'
+version '0.5.0'
 
 lua54 'yes'
 
 shared_script '@ox_lib/init.lua'
-
 client_script 'client/main.lua'
 server_script 'server/main.lua'
 
