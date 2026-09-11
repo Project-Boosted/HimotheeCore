b3bf0f95d0d57c17ddb776bcdf63bc8d35ad14dd
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:08:02 +0100

    feat: add qb-input compatibility facade

---
+++ resources/[himo]/qb-input/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'qb-input'
+author 'HimotheeGaming'
+description 'qb-input compatibility facade backed by ox_lib input dialogs'
+version '0.5.0'
+
+lua54 'yes'
+shared_script '@ox_lib/init.lua'
+client_script 'client/main.lua'
+
+dependency 'ox_lib'
