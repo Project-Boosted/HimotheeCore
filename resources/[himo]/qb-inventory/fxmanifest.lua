501ea45b1d55fffb712db3dd2dad738106175604
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:10:05 +0100

    feat: add qb-inventory compatibility facade

---
+++ resources/[himo]/qb-inventory/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'qb-inventory'
+author 'HimotheeGaming'
+description 'qb-inventory compatibility facade backed by ox_inventory'
+version '0.5.0'
+
+lua54 'yes'
+shared_script '@ox_lib/init.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+dependencies {
+    'ox_lib',
+    'ox_inventory'
+}
