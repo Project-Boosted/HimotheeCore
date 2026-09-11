746b9c4c33001992083d0a3fd0566f6677f714d5
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 11:03:36 +0100

    release: bump himo_qb_bridge to v0.5.5

--- resources/[himo]/himo_qb_bridge/fxmanifest.lua
+++ resources/[himo]/himo_qb_bridge/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'himo_qb_bridge'
 author 'HimotheeGaming'
 description 'Internal QB compatibility engine backed by HimotheeCore native services'
-version '0.5.4'
+version '0.5.5'
 
 lua54 'yes'
 
