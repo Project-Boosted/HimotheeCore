af7e57e18b84100cf20072c3cc273f63a8ae411b
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 01:05:17 +0100

    refactor: move qb-core identity to real compatibility facade

--- resources/[himo]/himo_qb_bridge/fxmanifest.lua
+++ resources/[himo]/himo_qb_bridge/fxmanifest.lua
@@ -3,12 +3,10 @@ game 'gta5'
 
 name 'himo_qb_bridge'
 author 'HimotheeGaming'
-description 'Limited QB compatibility bridge backed by HimotheeCore native services'
-version '0.4.2'
+description 'Internal QB compatibility engine backed by HimotheeCore native services'
+version '0.4.3'
 
 lua54 'yes'
-
-provide 'qb-core'
 
 shared_script '@ox_lib/init.lua'
 client_script 'client/main.lua'
