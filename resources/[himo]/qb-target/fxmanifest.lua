6be53baff4194dd3142ca79b4bf4d51b86127e09
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-11 02:09:07 +0100

    feat: add qb-target compatibility facade

---
+++ resources/[himo]/qb-target/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'qb-target'
+author 'HimotheeGaming'
+description 'qb-target compatibility facade backed by ox_target'
+version '0.5.0'
+
+lua54 'yes'
+shared_script '@ox_lib/init.lua'
+client_script 'client/main.lua'
+
+dependencies {
+    'ox_lib',
+    'ox_target'
+}
