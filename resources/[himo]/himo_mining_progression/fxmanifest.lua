6109ba481a4a39d49fe448bd5e18bd7d05e2add5
Project boosted <99775624+Project-Boosted@users.noreply.github.com>
2026-09-12 22:38:18 +0100

    feat(mining): add progression resource manifest

---
+++ resources/[himo]/himo_mining_progression/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'himo_mining_progression'
+author 'HimotheeGaming'
+description 'Persistent skill progression companion for jim-mining'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'ox_inventory',
+    'qbx_core',
+    'jim-mining'
+}
